968b472406724610b3007d76d16440ff7e304489
= <=>
2023-01-04 11:19:21 -0300

    adding new components

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,17 +1,46 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Button, Buttonprops } from '@ignite-ui/react'
-
+import { Button, ButtonProps } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
 export default {
   title: 'Form/Button',
   component: Button,
   args: {
-    children: 'Enviar',
+    children: 'Send',
   },
-} as Meta<Buttonprops>
+} as Meta<ButtonProps>
 
-export const Primary: StoryObj<Buttonprops> = {}
-export const Big: StoryObj<Buttonprops> = {
+export const Primary: StoryObj<ButtonProps> = {}
+export const Secondary: StoryObj<ButtonProps> = {
   args: {
-    size: 'big',
+    variant: 'secondary',
+    children: 'Create new',
+  },
+}
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  },
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+  },
+}
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    ),
   },
 }
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur vitae iste rem, quos dolores sit odit quo hic. Doloremque iste blanditiis quis eligendi quibusdam totam reiciendis velit labore laborum voluptates!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
